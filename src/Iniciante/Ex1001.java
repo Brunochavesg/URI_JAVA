bd9e41849d0e92e38a0e30298a07bbe80baf9b57
Bruno Chaves <brunochavesg@gmail.com>
2020-07-04 22:56:11 -0300

    Initial commit

---
+++ src/Iniciante/Ex1001.java
@@ -0,0 +1,16 @@
+package Iniciante;
+
+public class Ex1001 {
+	
+	public static void main(String[] args) {
+		
+		int A = 10;
+		int B = 9;
+		int X = (A + B);
+		
+		System.out.println("X = " + X);
+		
+		
+	}
+
+}
